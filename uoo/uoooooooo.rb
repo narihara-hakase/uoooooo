0c3c95ca99fbe692a057010aff265c197cb9b05d
Narihara <plasmazanberbreaker@gmail.com>
2019-04-19 18:27:04 +0900

    うお言語

---
+++ uoo/uoooooooo.rb
@@ -0,0 +1,15 @@
+Encoding.default_external = "UTF-8"
+require 'r-fxxk'
+
+class Uooooo < Brainfuck
+  nxt "うお！"
+  inc "うおおおおお！"
+  prv "うおおお！"
+  dec "解散"
+  opn "借金"
+  cls "取り立て"
+  put "うお…"
+  get "教会"
+end
+
+puts Uooooo.new.fuck(ARGF.read)
